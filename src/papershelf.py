7a9d5084bdf5b45616b1fe06f3e745c3e014e619
Ming Liu <Ming.Aaron.Liu@gmail.com>
2015-12-25 22:44:43 -0500

    sync

--- src/papershelf.py
+++ src/papershelf.py
@@ -1,35 +1,40 @@
 #!/usr/bin/python
-"""
-Status: on-going
-"""
 
 import argparse
 from item import *
 
-parser = argparse.ArgumentParser(description = 'A simply paper organization tool')
-parser.add_argument('command', choices = ['configure', 'add', 'remove', 'show'],
-		    help = 'Four commands supported by papershelf')
+parser = argparse.ArgumentParser(description = 'A simple paper organization tool',
+				 formatter_class = argparse.RawTextHelpFormatter)
+parser.add_argument("command", choices = ['configure', 'add', 'remove', 'show'],
+		    help = 'Four commands supported by papershelf\n'
+		           'Options supported by configure: -storage\n'
+			   'Options supported by add:-a -f -s -p -n -t -y -c -d\n'
+			   'Options supported by remove: -a -f -s -p -n\n'
+			   'Options supported by show: -a -f -s -p\n')
+parser.add_argument('-storage', '--papershelf storage',
+		    help = 'Move recorded papers into this storage')
 parser.add_argument('-a', '--area', default = 'computer science',
-		    help = 'Research area in the papershelf')
+		    help = 'Research area in the specified papershelf')
 parser.add_argument('-f', '--field',
 		    help = 'Field in the specified research area')
 parser.add_argument('-s', '--subfield',
                     help = 'Subfield in the specified field')
 parser.add_argument('-p', '--problem',
 		    help = 'Problem in the subfiled')
+parser.add_argument('-n', '--name',
+                    help = 'Paper file name')
 parser.add_argument('-t', '--title',
 		    help = 'Paper title')
 parser.add_argument('-y', '--year', type = int,
 		    help = 'Paper published year')
 parser.add_argument('-c', '--conference/journal/others',
 		    help = 'Paper published conference/journal/others')
-parser.add_argument('-n', '--notes',
+parser.add_argument('-d', '--description',
 		    help = 'Write some important notes')
-
 args = parser.parse_args()
 
 class papershelf(item):
 	pass
 
 if __name__ == "__main__":
-	print "Hello world!"
+	print args
